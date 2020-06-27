ada02af9c82ed3264b7df925ab15aa9ca4b9e3e8
Bruno Viveiros <bruferviveiros@gmail.com>
2020-06-27 10:43:55 -0300

    Removendo warnings

--- web/src/pages/Profile/index.tsx
+++ web/src/pages/Profile/index.tsx
@@ -109,7 +109,7 @@ const Profile: React.FC = () => {
         });
       }
     },
-    [addToast, history],
+    [addToast, history, updateUser],
   );
 
   const handleAvatarChange = useCallback(
@@ -129,7 +129,7 @@ const Profile: React.FC = () => {
         });
       }
     },
-    [addToast],
+    [addToast, updateUser],
   );
 
   return (
